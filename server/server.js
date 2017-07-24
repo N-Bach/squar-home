fd9a5d87aa80aa0f9016b0b02661553b50e348d5
Nguyen Bach <nguyenbach2810@gmail.com>
2017-07-24 14:36:51 +0700

    first commit

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+var app = express();
+
+const PORT = process.env.PORT || 3000;
+
+const publicPath = path.join(__dirname, '../public');
+
+app.use(express.static(publicPath));
+
+app.get('/', (req, res) => {
+  res.send('index.html');
+});
+
+app.listen(PORT, () => {
+  console.log('Server is listenning at', PORT);
+});
