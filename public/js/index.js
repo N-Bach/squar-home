f12b2429747851ee05c0d642d479e2f85a4a4be3
Nguyen Bach <nguyenbach2810@gmail.com>
2017-07-27 09:36:23 +0700

    move icon folder to img folder, refactor code

--- public/js/index.js
+++ public/js/index.js
@@ -13,33 +13,33 @@ function srcollDown() {
 }
 
 function switchThemeDark(index) {
-  $('#ic-hamburger').attr('src', 'icon/ic-menu-dark.png');
-  $('#scroll-down').attr('src', 'icon/ic-animated-scroll-yellow.gif');
+  $('#ic-hamburger').attr('src', 'img/icon/ic-menu-dark.png');
+  $('#scroll-down').attr('src', 'img/icon/ic-animated-scroll-yellow.gif');
   $('#text-scroll-down').css('color', 'black');
-  $('#fp-nav').find('span').css('background-image', 'url(../../icon/ic-unactive-dark.png)');
-  $('#fp-nav').find('span').css('background-image', 'url(icon/ic-slider-unactive-dark.png)');
+  // $('#fp-nav').find('span').css('background-image', 'url(../../icon/ic-unactive-dark.png)');
+  $('#fp-nav').find('span').css('background-image', 'url(img/icon/ic-slider-unactive-dark.png)');
   $('#fp-nav li:nth-child(' + index + ')')
     .find('span')
-    .css('background-image', 'url(icon/ic-slider-active.png)');
+    .css('background-image', 'url(img/icon/ic-slider-active.png)');
 }
 
 function switchThemeLight() {
-  $('#ic-hamburger').attr('src', 'icon/ic-menu.png');
-  $('#scroll-down').attr('src', 'icon/ic-animated-scroll.gif');
+  $('#ic-hamburger').attr('src', 'img/icon/ic-menu.png');
+  $('#scroll-down').attr('src', 'img/icon/ic-animated-scroll.gif');
   $('#text-scroll-down').css('color', 'white');
-  $('#fp-nav').find('span').css('background-image', 'url(icon/ic-slider-unactive.png)');
+  $('#fp-nav').find('span').css('background-image', 'url(img/icon/ic-slider-unactive.png)');
   $('#fp-nav li:first-child')
     .find('span')
-    .css('background-image', 'url(icon/ic-slider-active.png)');
+    .css('background-image', 'url(img/icon/ic-slider-active.png)');
 }
 
 function updateThemeDark(index, nextIndex) {
   $('#fp-nav li:nth-child(' + index + ')')
     .find('span')
-    .css('background-image', 'url(icon/ic-slider-unactive-dark.png)');
+    .css('background-image', 'url(img/icon/ic-slider-unactive-dark.png)');
   $('#fp-nav li:nth-child(' + nextIndex + ')')
     .find('span')
-    .css('background-image', 'url(icon/ic-slider-active.png)');
+    .css('background-image', 'url(img/icon/ic-slider-active.png)');
 }
 
 $(document).ready(function() {
