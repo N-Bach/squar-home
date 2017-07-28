f27ebca22eff48a4ae0ed3752b7067def799d656
Nguyen Bach <nguyenbach2810@gmail.com>
2017-07-28 14:24:00 +0700

    add section 5

--- public/js/index.js
+++ public/js/index.js
@@ -16,7 +16,6 @@ function switchThemeDark(index) {
   $('#ic-hamburger').attr('src', 'img/icon/ic-menu-dark.png');
   $('#scroll-down').attr('src', 'img/icon/ic-animated-scroll-yellow.gif');
   $('#text-scroll-down').css('color', 'black');
-  // $('#fp-nav').find('span').css('background-image', 'url(../../icon/ic-unactive-dark.png)');
   $('#fp-nav').find('span').css('background-image', 'url(img/icon/ic-slider-unactive-dark.png)');
   $('#fp-nav li:nth-child(' + index + ')')
     .find('span')
@@ -42,12 +41,19 @@ function updateThemeDark(index, nextIndex) {
     .css('background-image', 'url(img/icon/ic-slider-active.png)');
 }
 
+function updateScrollText(text) {
+  $('#text-scroll-down').html(text);
+}
+
 $(document).ready(function() {
+  var $isAnimatedSecond = $('#section2 .is-animated');
+  var $isAnimatedFifth = $('#section5 .is-animated');
   $('#fullpage').fullpage({
     navigation: true,
     navigationPosition: 'right',
     scrollingSpeed: 550,
     onLeave: function(index, nextIndex, direction) {
+      closeNav();
       var leavingSection = $(this);
 
       if (nextIndex === 1) {
@@ -58,6 +64,34 @@ $(document).ready(function() {
         } else {
           updateThemeDark(index, nextIndex);
         }
+      }
+
+      if (nextIndex === 2) {
+        $isAnimatedSecond.eq(0).addClass('animated fadeIn').css('animation-delay', '.4s');
+        $isAnimatedSecond.eq(1).addClass('animated fadeIn').css('animation-delay', '.6s');
+        $isAnimatedSecond.eq(2).addClass('animated fadeIn').css('animation-delay', '.7s');
+        $isAnimatedSecond.eq(3).addClass('animated fadeIn').css('animation-delay', '.8s');
+        $isAnimatedSecond.eq(4).addClass('animated fadeIn').css('animation-delay', '1s');
+      }
+
+      if (nextIndex === 5) {
+        $isAnimatedFifth.eq(0).addClass('animated fadeIn').css('animation-delay', '.5s');
+        $isAnimatedFifth.eq(1).addClass('animated fadeIn').css('animation-delay', '.7s');
+        // jobs
+        $isAnimatedFifth.eq(2).addClass('animated fadeIn').css('animation-delay', '1s');
+        $isAnimatedFifth.eq(3).addClass('animated fadeIn').css('animation-delay', '1.2s');
+        $isAnimatedFifth.eq(4).addClass('animated fadeIn').css('animation-delay', '1.3s');
+        $isAnimatedFifth.eq(5).addClass('animated fadeIn').css('animation-delay', '1.4s');
+        $isAnimatedFifth.eq(6).addClass('animated fadeIn').css('animation-delay', '1.5s');
+        $isAnimatedFifth.eq(7).addClass('animated fadeIn').css('animation-delay', '1.6s');
+        $isAnimatedFifth.eq(8).addClass('animated fadeIn').css('animation-delay', '1.7s');
+        $isAnimatedFifth.eq(9).addClass('animated fadeIn').css('animation-delay', '1.8s');
+      }
+
+      if (nextIndex === 5) {
+        updateScrollText('Scroll Up');
+      } else if (index === 5) {
+        updateScrollText('Scroll Down');
       }
     }
   });
