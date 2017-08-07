556e9641d3dcdf6d9232b91e5bfe6ad1175d9059
Nguyen Bach <nguyenbach2810@gmail.com>
2017-08-07 18:17:38 +0700

    add links to product page

--- public/js/products-index.js
+++ public/js/products-index.js
@@ -7,3 +7,8 @@ function openNav() {
 function closeNav() {
   document.getElementById('mySidenav').style.width = '0';
 }
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Chrome, Safari and Opera
+  document.documentElement.scrollTop = 0; // For IE and Firefox
+}
