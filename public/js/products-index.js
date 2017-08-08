0b8f3349ae514a1497a4b37300dd6f131d8c3a17
Nguyen Bach <nguyenbach2810@gmail.com>
2017-08-08 15:16:01 +0700

    add sticky nav bar

--- public/js/products-index.js
+++ public/js/products-index.js
@@ -12,3 +12,35 @@ function topFunction() {
   document.body.scrollTop = 0; // For Chrome, Safari and Opera
   document.documentElement.scrollTop = 0; // For IE and Firefox
 }
+
+var nav = $('#nav');
+var sticky = 'sticky';
+var header = $('.header').height();
+var container = $('.container');
+var breadcrumb = $('.breadcrum-container');
+var hamburger = $('#hamburger');
+
+// console.log(nav);
+// console.log(header);
+
+$(window).scroll(function() {
+  if ($(this).scrollTop() > header) {
+    nav.addClass(sticky);
+    container.css('margin-top', '62px');
+
+    breadcrumb.addClass('sticky-crumb');
+    $('.ic-home').attr('src', 'img/icon/ic-home.png');
+
+    hamburger.addClass('sticky-sidenav');
+    $('#ic-hamburger').attr('src', 'img/icon/ic-menu-dark.png');
+  } else {
+    nav.removeClass(sticky);
+    container.css('margin-top', '0px');
+
+    breadcrumb.removeClass('sticky-crumb');
+    $('.ic-home').attr('src', 'img/icon/ic-home-white.png');
+
+    hamburger.removeClass('sticky-sidenav');
+    $('#ic-hamburger').attr('src', 'img/icon/ic-menu.png');
+  }
+});
