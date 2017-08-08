12471c383329c2f563fadb62b3bc6d278fef641b
Nguyen Bach <nguyenbach2810@gmail.com>
2017-08-08 16:30:26 +0700

    add tabs

--- public/js/products-index.js
+++ public/js/products-index.js
@@ -20,9 +20,7 @@ var container = $('.container');
 var breadcrumb = $('.breadcrum-container');
 var hamburger = $('#hamburger');
 
-// console.log(nav);
-// console.log(header);
-
+// sticky nav
 $(window).scroll(function() {
   if ($(this).scrollTop() > header) {
     nav.addClass(sticky);
@@ -44,3 +42,24 @@ $(window).scroll(function() {
     $('#ic-hamburger').attr('src', 'img/icon/ic-menu.png');
   }
 });
+
+function openCity(evt, cityName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName('tabcontent');
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = 'none';
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName('tablinks');
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(' active', '');
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(cityName).style.display = 'block';
+  evt.currentTarget.className += ' active';
+}
