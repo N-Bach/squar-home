7708f92d4a634c2ba6f35c2ebbcb8a79b8aa4e05
Nguyen Bach <nguyenbach2810@gmail.com>
2017-10-10 18:20:56 +0700

    add singapore office address in contact us

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+var bodyParser = require('body-parser');
 var app = express();
 
 const PORT = process.env.PORT || 3000;
@@ -7,6 +8,16 @@ const PORT = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicPath));
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+  req.header('Access-Control-Allow-Origin', '*');
+  req.header('Access-Control-Allow-Headers', 'X-Requested-With');
+  next();
+});
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.send('index.html');
@@ -24,9 +35,9 @@ app.get('/products/callhome', (req, res) => {
   res.sendFile(path.join(publicPath + '/callhome.html'));
 });
 
-app.get('/products/fastsell', (req, res) => {
-  res.sendFile(path.join(publicPath + '/fastsell.html'));
-});
+// app.get('/products/fastsell', (req, res) => {
+//   res.sendFile(path.join(publicPath + '/fastsell.html'));
+// });
 
 app.get('/products/mingalarbarmorning', (req, res) => {
   res.sendFile(path.join(publicPath + '/mingalarbarmorning.html'));
@@ -70,6 +81,26 @@ app.get('/terms-of-service', (req, res) => {
 
 app.get('/test-game', (req, res) => {
   res.sendFile(path.join(publicPath + '/test-game.html'));
+});
+
+app.post('/game/score', (req, res) => {
+  console.log(req.method);
+  console.log(req.baseUrl);
+  console.log('----------------> Header');
+  console.log(req.headers);
+  console.log('----------------> Params');
+  console.log(req.params);
+  console.log('----------------> Query');
+  console.log(req.query);
+  console.log('----------------> body');
+  if (req.body) {
+    console.log(req.body);
+    console.log('the score is: ', req.body.score);
+    console.log('message is: ', req.body.mes);
+  } else {
+    console.log('no body in request');
+  }
+  res.send(JSON.stringify({ data: 12 }));
 });
 
 app.listen(PORT, () => {
